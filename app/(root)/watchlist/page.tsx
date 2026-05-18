c6818960a0f9ccf4dafe718c2460ddcd70ebe666
Chaitanya Sai <chaitanyasaisivagoni@gmail.com>
2026-05-18 16:03:10 +0530

    Bug fix

--- app/(root)/watchlist/page.tsx
+++ app/(root)/watchlist/page.tsx
@@ -3,7 +3,7 @@ import { searchStocks } from '@/lib/actions/finnhub.actions';
 import SearchCommand from '@/components/SearchCommand';
 import { getWatchlistWithData } from '@/lib/actions/watchlist.actions';
 import { WatchlistTable } from '@/components/WatchlistTable';
-
+export const dynamic = 'force-dynamic';
 const Watchlist = async () => {
     const watchlist = await getWatchlistWithData();
     const initialStocks = await searchStocks();
